7a1915674e4275f1936de6af56b6d2096e6385d9
Renato Cordova <ren99cordova@gmail.com>
2023-04-18 18:43:49 -0400

    finished with the navbar, working on login/signup page next

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+  display: "flex",
+  justifyConstent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
